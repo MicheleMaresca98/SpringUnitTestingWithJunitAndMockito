8c4d8e08415e6fdeb12402af1d3160e8b17f18a7
m.maresca <m.maresca@namirial.com>
2025-02-23 19:16:57 +0100

    refactor: tests with mock using mockito

--- unit-testing/src/test/java/com/in28minutes/unittesting/unit_testing/business/SomeBusinessMockTest.java
+++ unit-testing/src/test/java/com/in28minutes/unittesting/unit_testing/business/SomeBusinessMockTest.java
@@ -1,24 +1,24 @@
 package com.in28minutes.unittesting.unit_testing.business;
 
 import com.in28minutes.unittesting.unit_testing.data.SomeDataService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessMockTest {
 
-    SomeBusinessImpl business = new SomeBusinessImpl();
-    SomeDataService dataServiceMock = mock(SomeDataService.class);
+    @InjectMocks
+    SomeBusinessImpl business;
 
-    @BeforeEach
-    public void before() {
-        business.setSomeDataService(dataServiceMock);
-    }
-
+    @Mock
+    SomeDataService dataServiceMock;
 
     @Test
     public void calculateSumUsingDataService_basic() {
