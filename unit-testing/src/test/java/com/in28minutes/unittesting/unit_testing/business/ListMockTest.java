c3c0ecb85cdfcb66356524545f614250459a96f9
m.maresca <m.maresca@namirial.com>
2025-02-24 09:50:25 +0100

    test: tests verification using argument-captor for multiple values with mockito

--- unit-testing/src/test/java/com/in28minutes/unittesting/unit_testing/business/ListMockTest.java
+++ unit-testing/src/test/java/com/in28minutes/unittesting/unit_testing/business/ListMockTest.java
@@ -64,8 +64,23 @@ public class ListMockTest {
         // Verification
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
         verify(mock).add(captor.capture());
-        
+
         assertEquals("SomeString", captor.getValue());
+    }
+
+    @Test
+    public void multipleArgumentCapturing() {
+        // SUT
+        mock.add("SomeString1");
+        mock.add("SomeString2");
+
+        // Verification
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mock, times(2)).add(captor.capture());
+
+        List<String> allValues = captor.getAllValues();
+        assertEquals("SomeString1", allValues.get(0));
+        assertEquals("SomeString2", allValues.get(1));
     }
 
 }
