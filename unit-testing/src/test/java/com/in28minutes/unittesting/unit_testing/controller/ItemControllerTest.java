b60003ff325777862d8c8d82e568d45e939c234a
m.maresca <m.maresca@namirial.com>
2025-03-09 16:41:42 +0100

    test: retrieveAllItems api

--- unit-testing/src/test/java/com/in28minutes/unittesting/unit_testing/controller/ItemControllerTest.java
+++ unit-testing/src/test/java/com/in28minutes/unittesting/unit_testing/controller/ItemControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,7 +41,6 @@ public class ItemControllerTest {
 
     @Test
     public void itemFromBusinessService_basic() throws Exception {
-
         when(businessService.retrieveHardcodedItem()).thenReturn(new Item(1, "Ball", 10, 100));
 
         RequestBuilder request = MockMvcRequestBuilders
@@ -48,6 +49,23 @@ public class ItemControllerTest {
         MvcResult result = mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}"))
+                .andReturn();
+    }
+
+    @Test
+    public void retrieveAllItems_basic() throws Exception {
+        when(businessService.retrieveAllItems()).thenReturn(
+                Arrays.asList(
+                        new Item(2, "Item2", 10, 10),
+                        new Item(3, "Item3", 20, 20)
+                ));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/all-items-from-database")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{id:2, name:Item2, price:10, quantity:10},{id:3, name:Item3, price:20, quantity:20}]"))
                 .andReturn();
     }
 
