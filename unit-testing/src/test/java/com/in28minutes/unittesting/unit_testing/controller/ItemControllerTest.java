207cd3b8cbb98e569813d5e43713d088f8ffa1b8
m.maresca <m.maresca@namirial.com>
2025-03-09 15:30:58 +0100

    test: item-from-business-service api

--- unit-testing/src/test/java/com/in28minutes/unittesting/unit_testing/controller/ItemControllerTest.java
+++ unit-testing/src/test/java/com/in28minutes/unittesting/unit_testing/controller/ItemControllerTest.java
@@ -1,14 +1,18 @@
 package com.in28minutes.unittesting.unit_testing.controller;
 
+import com.in28minutes.unittesting.unit_testing.business.ItemBusinessService;
+import com.in28minutes.unittesting.unit_testing.model.Item;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,10 +23,27 @@ public class ItemControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private ItemBusinessService businessService;
+
     @Test
     public void dummyItem_basic() throws Exception {
         RequestBuilder request = MockMvcRequestBuilders
                 .get("/dummy-item")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}"))
+                .andReturn();
+    }
+
+    @Test
+    public void itemFromBusinessService_basic() throws Exception {
+
+        when(businessService.retrieveHardcodedItem()).thenReturn(new Item(1, "Ball", 10, 100));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/item-from-business-service")
                 .accept(MediaType.APPLICATION_JSON);
         MvcResult result = mockMvc.perform(request)
                 .andExpect(status().isOk())
